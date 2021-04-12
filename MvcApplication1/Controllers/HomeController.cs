bd8bd540badf05a74c134efe56782276d3fa07c8
abhineet-kumar <abhineet.knit@gmail.com>
2021-04-12 12:06:47 +0530

    WebApplication
    WebApplication

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestDll;
+
+namespace MvcApplication1.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        string te = test.Aa();
+        public ActionResult Index()
+        {
+            ViewData["Message"] = te;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
